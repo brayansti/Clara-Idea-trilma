2c256bb86b49144c0e9d2256bcdfaf96970a7e9f
Brayan Camargo <brayan440@hotmail.com>
2019-08-20 17:41:33 -0300

    Front-end

--- deploy/index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php include 'includes/head.php'; ?>
-<body>
-	<?php include 'includes/header.php'; ?>
-	<?php include 'includes/footer.php'; ?>
-</body>
-</html>
